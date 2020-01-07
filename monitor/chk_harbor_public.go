4f569daaad8e3a470ebe898fd36ecd3411fb8eb5
lijinghuatongxue <598820383@qq.com>
2020-01-07 09:06:03 +0800

    [add]Harbor 公开仓库检查，null则为正常，目前没有公开的仓库，非null则有

---
+++ monitor/chk_harbor_public.go
@@ -0,0 +1,25 @@
+package main
+
+//  Harbor 公开仓库检查，null则为正常，目前没有公开的仓库，非null则有
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	//"os"
+)
+
+//var url = "https://harbor.xxx.net/api/projects?project_name=guest"
+
+func main() {
+	response, err := http.Get(os.Args[1]+"/api/projects?project_name=guest")
+	//response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("url error")
+		defer response.Body.Close()
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
+
